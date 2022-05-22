4b70012c0423b21f5b513b05d378bad13fd809b2
Savlar <matusgal@outlook.com>
2022-05-22 14:43:07 +0200

    rewritten file

--- main.py
+++ main.py
@@ -1,65 +1,105 @@
+import os
 import matplotlib.pyplot as plt
 import numpy
 
-with open('data.TXT', 'r') as file:
-    # file = file.read().split('\n')[:-1]
-    file = file.read().split('\n')
-file = [[float(x) for x in i.split()] for i in file]
 
-stlpce = [[] for _ in range(4)]
-for row in file:
-    for i in range(len(row)):
-        stlpce[i].append(row[i])
+fig = plt.figure()
+ax = fig.add_subplot(111)
 
-fig, ax = plt.subplots()
 
-pom3 = [i for i in range(len(stlpce[1])) if stlpce[1][i] < -1.8]
+def get_data(filename):
+    with open(filename, 'r') as input_:
+        input_ = input_.read().split('\n')
+    if len(input_[-1]) == 0:
+        input_ = input_[:-1]
+    return [[float(val) for val in row.split()] for row in input_]
 
-pom4 = []
-for i in range(len(pom3) - 1):
-    if pom3[i + 1] - pom3[i] > 10:
-        pom4.append(pom3[i])
 
-cyklus = []
-for i in range(0, len(pom4) - 2, 2):
-    cyklus.append(stlpce[2][pom4[i]:pom4[i + 2]+1])
+def get_cycle(lst, hu):
+    return [lst[hu[i]:hu[i + 2]+1] for i in range(0, len(hu) - 2, 2)]
 
-pom5 = min([len(row) for row in cyklus])
-pom6 = [row[:pom5] for row in cyklus]
 
-priemernehodnotydruhystlpec = [sum([row[i] for row in pom6]) / len(pom6) for i in range(pom5)]
+def get_cycle_starts(cols):
+    pom3 = [i for i in range(len(cols[1])) if cols[1][i] < -1.8]
 
-cyklus3 = [stlpce[2][pom4[i]:pom4[i + 2]+1] for i in range(0, len(pom4) - 2, 2)]
-cyklus4 = [stlpce[3][pom4[i]:pom4[i + 2]+1] for i in range(0, len(pom4) - 2, 2)]
+    hu = []
+    for i in range(len(pom3) - 1):
+        if pom3[i + 1] - pom3[i] > 10:
+            hu.append(pom3[i])
+    return hu
 
-pom5 = min([len(row) for row in cyklus3])
 
-cyklus3orezany = [cyklus3[i][:pom5] for i in range(len(cyklus3))]
-cyklus4orezany = [cyklus4[i][:pom5] for i in range(len(cyklus3))]
+def slice_(lst, length):
+    return [lst[i][:length] for i in range(len(lst))]
 
-priemernehodnoty3stlpec = [sum([row[i] for row in cyklus3orezany]) / len(pom6) + 1.1335 for i in range(pom5)]
-priemernehodnoty4stlpec = [sum([row[i] for row in cyklus4orezany]) / len(pom6) for i in range(pom5)]
 
-k = 0
-x = []
-for i in range(len(priemernehodnoty3stlpec)):
-    k += 720 / len(priemernehodnoty3stlpec)
-    x.append(k)
+def get_avg(lst, length, correction=0.):
+    return [sum([row[i] for row in lst]) / len(lst) + correction for i in range(length)]
 
-a = numpy.array(list(zip(priemernehodnoty3stlpec, x)))
-xd = numpy.matrix.transpose(a).tolist()
-# ax.plot(priemernehodnotydruhystlpec)
-ax.plot(xd[1], xd[0])
 
-avg = sum(priemernehodnoty3stlpec[1155:1292]) / (1292-1155)
+def get_delta(lst):
+    d = []
+    delta_angle = 720 / len(lst[0])
+    for cycle in lst:
+        start = int((len(cycle) / 720) * 310)
+        end = 1 + int((len(cycle) / 720) * 410)
+        d.append(max(((cycle[i + 1] - cycle[i]) / delta_angle) for i in range(start, end)))
+    return sum(d) / len(d)
 
-# ax.plot(priemernehodnoty4stlpec)
-# plt.xlabel('crank angle')
-# plt.ylabel('pressure')
-# plt.ylim([0.8, 1.5])
-# plt.show()
-plt.vlines(216, 0, 7, colors=['red'], label='SZ')
-plt.vlines(360, 0, 7, colors=['black'], label='HÚ')
-plt.vlines(507, 0, 7, colors=['yellow'], label='VO')
-plt.legend()
-plt.savefig('test.jpg')
+
+def get_constant(lst):
+    p = []
+    for row in lst:
+        start = int((len(row) / 720) * 170)
+        end = 1 + int((len(row) / 720) * 190)
+        p_atm = 0.101325
+        p.append((sum(row[start:end]) / (end - start)) - p_atm)
+    return sum(p) / len(p)
+
+
+def run():
+    for filename in os.listdir('files'):
+        file = get_data('files/' + filename)
+        cols = list(zip(*file))
+        pom4 = get_cycle_starts(cols)
+        if not (-1 <= cols[2][pom4[0]] <= 1):
+            pom4 = pom4[1:]
+        min_length = min([pom4[i + 2] - pom4[i] for i in range(0, len(pom4) - 2, 2)]) + 1
+
+        cyklus2orezany = slice_(get_cycle(cols[2], pom4), min_length)
+        cyklus3orezany = slice_(get_cycle(cols[2], pom4), min_length)
+        cyklus4orezany = slice_(get_cycle(cols[3], pom4), min_length)
+        delta = get_delta(cyklus3orezany)
+
+        c = abs(get_constant(cyklus3orezany))
+
+        priemernehodnoty2stlpec = get_avg(cyklus2orezany, min_length)
+        priemernehodnoty3stlpec = get_avg(cyklus3orezany, min_length, c)
+        priemernehodnoty4stlpec = get_avg(cyklus4orezany, min_length)
+
+        x = numpy.linspace(0, 720, num=len(priemernehodnoty3stlpec))
+        a = numpy.array(list(zip(x, priemernehodnoty3stlpec)))
+        a = numpy.matrix.transpose(a).tolist()
+        ax.plot(*a, label=filename)
+        print(filename, delta)
+
+
+if __name__ == '__main__':
+    run()
+    # plt.ylim([0.8, 1.5])
+    plt.vlines(216, 0, 7, colors=['blue'])
+    plt.vlines(360, 0, 7, colors=['black'])
+    plt.vlines(507, 0, 7, colors=['red'])
+    plt.vlines(720, 0, 7, colors=['black'], label='Horná úvrať')
+    plt.vlines(180, 0, 7, colors=['green'])
+    plt.vlines(540, 0, 7, colors=['green'], label='Dolná úvrať')
+    plt.vlines(22, 0, 7, colors=['red'], label='Výfukový ventil')
+    plt.vlines(704, 0, 7, colors=['blue'], label='Sací ventil')
+    plt.xlabel('Uhol natočenia KH (º)')
+    plt.ylabel('Tlak (MPa)')
+    plt.title('p - α diagram')
+    plt.xticks([*range(90, 721, 90)] + [22, 216, 507], fontsize=7.5)
+    legend = ax.legend(loc='upper center', bbox_to_anchor=(1.35, 0.9))
+    plt.savefig('output.jpg', bbox_extra_artists=(legend, ), bbox_inches='tight')
+
+    # avg = sum(priemernehodnoty3stlpec[1255:1402]) / (1402-1255)
